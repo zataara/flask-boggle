5b437b50166e4f8c184ef0de83c3be23b8ad830a
zataara <bryantmac@gmail.com>
2021-06-05 15:17:26 -0700

    adding initial routes

--- app.py
+++ app.py
@@ -1,3 +1,30 @@
+from flask import Flask, session, render_template, jsonify, redirect
 from boggle import Boggle
 
 boggle_game = Boggle()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'bOgGlEsEcReTs'
+
+@app.route('/')
+def make_board():
+    '''create the game board'''
+
+    board = boggle_game.make_board()
+
+    session['board'] = board
+
+    return render_template('index.html', board=board)
+
+@app.route('/check-word')
+def check_word():
+    '''checks to see if the guessed word is in words.txt'''
+
+
+    word = request.args['word']
+    board = session['board']
+    res = boggle_game.check_valid_word(board,word)
+
+    return jsonify({'result': res})
+
+
