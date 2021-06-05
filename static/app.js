049310c4a2677cf080deaeb3090a91425d578c8d
zataara <bryantmac@gmail.com>
2021-06-05 16:48:06 -0700

    added class methods

---
+++ static/app.js
@@ -0,0 +1,73 @@
+class BoggleGame {
+/* Create a new Boggle Game*/
+
+    constructor(boardId, secs = 60){
+        this.secs = secs; //Game length
+        this.score = 0;
+        this.words = new Set();
+        this.boardId = $('#' + boardId);
+        this.timer = setInterval(this.tick.bind(this), 1000);
+        this.showTimer();
+
+        $('.add-word', this.board).on('submit', this.handleSubmit.bind(this))
+    }
+
+    showWord(word){
+        //take valid submitted word and append to DOM as list element
+        $('.words', this.board).append($('<li>', {text: word}));
+    }
+
+    showScore(){
+        $('.score', this.board).text(this.score)
+    }
+
+    showMessage(msg, cls){
+        //show a message and add a class based upon user input parameters
+        $('.msg', this.board)
+            .text(msg)
+            .removeClass()
+            .addClass(`msg ${cls}`)
+    }
+
+    async handleSubmit(evt){
+        evt.preventDefault();
+        const word = $('.word', this.board).val();
+            //grab value of form input
+
+        if(!word){
+            //check to make sure text has been entered
+            return
+        }
+
+        if(this.word.has(word)){
+            //check words already entered
+            this.showMessage('Already found word', 'err')
+            return
+        }
+
+        const res = await axios.get('/check-word', {params: {word: word }});
+
+        if (res.data.result === 'not-word'){
+            this.showMessage(`Nice try! ${word} is not a word!`, 'err');
+        } else if (res.data.result === 'not-on-board'){
+            this.showMessage(`${word} is not on the game board!`, 'err');
+        } else {
+            this.showWord(word);
+            this.score += word.length
+            this.showScore();
+            this.words.add(word);
+            this.showMessage(`${word} has been added!`, 'ok')
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
